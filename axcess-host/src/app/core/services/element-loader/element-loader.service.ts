00eb26a7c3fd030a6ed00a3042870e70f496657f
pradipkumardesai <pradipkumarsemail@gmail.com>
2019-07-02 15:26:00 +0530

    clear inner html

--- axcess-host/src/app/core/services/element-loader/element-loader.service.ts
+++ axcess-host/src/app/core/services/element-loader/element-loader.service.ts
@@ -7,22 +7,25 @@ export class ElementLoaderService {
 
   constructor() { }
 
-  load(url:string,dvName:string,elName:string): void {//axcess-tax
-  
+  load(url: string, dvName: string, elName: string): void {//axcess-tax
+
     const content = document.getElementById(dvName);
+    this.clearInnerHTML(content);
     const script = document.createElement('script');
     script.src = url;
     script.onload = this.onScriptLoaded;
-
     content.appendChild(script);
     const webAppElement: HTMLElement = document.createElement(elName);
     content.appendChild(webAppElement);
     webAppElement.setAttribute('state', 'init');
   }
 
-  onScriptLoaded()
-  {
-    eval('delete window.webpackJsonp');    
+  onScriptLoaded() {
+    eval('delete window.webpackJsonp');
     console.log('loader false');
-  } 
+  }
+
+  clearInnerHTML(e: HTMLElement) {
+    e.innerHTML = "";
+  }
 }
