5aae6b2d0c882a58a093356664f36dd3b9c89c32
Pradipkumar Desai <pradipkumarsemail@gmail.com>
2019-07-09 10:52:55 +0000

    get links by auth token

--- axcess-host/src/app/app.module.ts
+++ axcess-host/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { CoreModule } from './core/core.module';
 import { routes } from './app.route.module';
 import { SharedModule } from './shared/shared.module';
 import { CookieService } from 'ngx-cookie-service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { CookieService } from 'ngx-cookie-service';
   imports: [
     BrowserModule,
     RouterModule.forRoot(routes),
+    HttpClientModule ,
     CoreModule,
     SharedModule
   ],
--- axcess-host/src/app/guards/authentication/authentication-service/auth.service.ts
+++ axcess-host/src/app/guards/authentication/authentication-service/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { CookieService } from 'ngx-cookie-service';
 import { ShellConfigService } from 'src/app/core/services/shell-config/shell-config.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -8,16 +9,30 @@ import { ShellConfigService } from 'src/app/core/services/shell-config/shell-con
 export class AuthService {
 
   private AUTH_ID: string;
+ 
 
-  constructor(private cookieService: CookieService, private shellConfigService: ShellConfigService) {
+  constructor(private cookieService: CookieService, private shellConfigService: ShellConfigService,private http: HttpClient) {
     this.AUTH_ID = this.shellConfigService.getAuthCookieName();
   }
 
   isLoggedIn(): boolean {
+    
     let autId: string = this.cookieService.get(this.AUTH_ID)
-    if (autId)
+    if (autId) {
+      alert(autId);
+      let httpOptions = {
+        headers: new HttpHeaders({
+          'Content-Type':  'application/json',
+          'Authorization': "Bearer "+autId
+        })
+      };
+           
+      this.http.get("https://z001test4commonservices.cchaxcess.com/CommonWebAPI/dashboardLinks/filteredlinksbygroup",httpOptions).subscribe(links=>{
+        alert(JSON.stringify(links))
+      });
       return true;
-    else
+
+    } else
       return false;
   }
 
