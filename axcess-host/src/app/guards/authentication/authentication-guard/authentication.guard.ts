364c2817fd51217d49e246134fdf97196663f280
Pradipkumar Desai <pradipkumarsemail@gmail.com>
2019-07-10 10:30:59 +0000

    load last url after login

--- axcess-host/src/app/guards/authentication/authentication-guard/authentication.guard.ts
+++ axcess-host/src/app/guards/authentication/authentication-guard/authentication.guard.ts
@@ -11,19 +11,26 @@ export class AuthGuard implements CanActivate {
   constructor(private authService: AuthService, private router: Router, private shellConfigService: ShellConfigService) { }
 
   canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
- 
+    debugger;
     let authTokQuryPrmName = this.shellConfigService.getAuthTokQuryPrmName();
     let url: string = state.url;
     let authTok: string = next.queryParams[authTokQuryPrmName];
+    let moduleName: string = next.params["id"];
 
-    if (authTok)
+    if (authTok) {
       this.authService.login(authTok);
+      if(localStorage.getItem("lastModuleId")){
+        this.router.navigateByUrl("/"+localStorage.getItem("lastModuleId"));
+        return false;
+      }
+    }
 
     let hasPerm: boolean = this.authService.isLoggedIn();
 
     if (hasPerm)
       return true;
     else {
+      localStorage.setItem("lastModuleId", moduleName);
       window.location.href = this.shellConfigService.getAuthServerURL();
       return false;
     }
