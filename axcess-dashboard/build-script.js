2012642b721ea61a6e8abccf0db152e89fcca805
Pradipkumar Desai <pradipkumarsemail@gmail.com>
2019-07-23 10:47:09 +0000

    dashboatrd app moved to v8

--- axcess-dashboard/build-script.js
+++ axcess-dashboard/build-script.js
@@ -4,8 +4,8 @@ const concat = require('concat');
 (async function build() {
 
     const files = [
-        './dist/axcess-dashboard/runtime.js',
-        './dist/axcess-dashboard/main.js'
+        './dist/axcess-dashboard/runtime-es2015.js',
+        './dist/axcess-dashboard/main-es2015.js'
     ]
 
     await fs.ensureDir('angularElements')
